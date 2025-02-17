174e2847be0bd3f76a5b870d303e5064b89f77da
Oğuz Çolak <114251468+oguzcoll@users.noreply.github.com>
2025-02-17 16:13:48 +0300

    animation with framer motion is completed to all sections

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,17 +1,31 @@
+'use client';
 import acmeLogo from '@/assets/logo-acme.png';
+
 import quantumLogo from '@/assets/logo-quantum.png';
 import echoLogo from '@/assets/logo-echo.png';
 import celestialLogo from '@/assets/logo-celestial.png';
 import pulseLogo from '@/assets/logo-pulse.png';
 import apexLogo from '@/assets/logo-apex.png';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 export const LogoTicker = () => {
   return (
     <div className='py-8 md:py-12 bg-white'>
       <div className='container'>
         {/* mask-image:  Creates a visual "fade" effect on scrolling content.*/}
         <div className='flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]'>
-          <div className='flex gap-14 flex-none'>
+          <motion.div
+            className='flex gap-14 flex-none pr-14'
+            animate={{
+              translateX: '-50%',
+            }}
+            transition={{
+              repeat: Infinity,
+              repeatType: 'loop',
+              duration: 10,
+              ease: 'linear',
+            }}
+          >
             <Image
               className='logo-ticker-image'
               src={acmeLogo}
@@ -52,7 +66,49 @@ export const LogoTicker = () => {
               src={quantumLogo}
               alt='Quantum Logo'
             />
-          </div>
+
+            {/* second turn of logos */}
+            <Image
+              className='logo-ticker-image'
+              src={acmeLogo}
+              alt='Acme Logo'
+            />
+            <Image
+              className='logo-ticker-image'
+              src={quantumLogo}
+              alt='Quantum Logo'
+            />
+            <Image
+              className='logo-ticker-image'
+              src={echoLogo}
+              alt='Echo Logo'
+            />
+            <Image
+              className='logo-ticker-image'
+              src={celestialLogo}
+              alt='Celestial Logo'
+            />
+            <Image
+              className='logo-ticker-image'
+              src={pulseLogo}
+              alt='Pulse Logo'
+            />
+            <Image
+              className='logo-ticker-image'
+              src={apexLogo}
+              alt='Apex Logo'
+            />
+            <Image
+              className='logo-ticker-image'
+              src={acmeLogo}
+              alt='Acme Logo'
+            />
+            <Image
+              className='logo-ticker-image'
+              src={quantumLogo}
+              alt='Quantum Logo'
+            />
+          </motion.div>
         </div>
       </div>
     </div>
