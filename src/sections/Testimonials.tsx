969420ab2b627f39861e7f52561f96a44eac1bca
Oğuz Çolak <114251468+oguzcoll@users.noreply.github.com>
2025-02-17 13:08:19 +0300

    testimonials section added

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,70 +1,135 @@
-import avatar1 from "@/assets/avatar-1.png";
-import avatar2 from "@/assets/avatar-2.png";
-import avatar3 from "@/assets/avatar-3.png";
-import avatar4 from "@/assets/avatar-4.png";
-import avatar5 from "@/assets/avatar-5.png";
-import avatar6 from "@/assets/avatar-6.png";
-import avatar7 from "@/assets/avatar-7.png";
-import avatar8 from "@/assets/avatar-8.png";
-import avatar9 from "@/assets/avatar-9.png";
+import avatar1 from '@/assets/avatar-1.png';
+import avatar2 from '@/assets/avatar-2.png';
+import avatar3 from '@/assets/avatar-3.png';
+import avatar4 from '@/assets/avatar-4.png';
+import avatar5 from '@/assets/avatar-5.png';
+import avatar6 from '@/assets/avatar-6.png';
+import avatar7 from '@/assets/avatar-7.png';
+import avatar8 from '@/assets/avatar-8.png';
+import avatar9 from '@/assets/avatar-9.png';
+import Image from 'next/image';
+import { twMerge } from 'tailwind-merge';
 
 const testimonials = [
   {
-    text: "As a seasoned designer always on the lookout for innovative tools, Framer.com instantly grabbed my attention.",
+    text: 'As a seasoned designer always on the lookout for innovative tools, Framer.com instantly grabbed my attention.',
     imageSrc: avatar1.src,
-    name: "Jamie Rivera",
-    username: "@jamietechguru00",
+    name: 'Jamie Rivera',
+    username: '@jamietechguru00',
   },
   {
     text: "Our team's productivity has skyrocketed since we started using this tool. ",
     imageSrc: avatar2.src,
-    name: "Josh Smith",
-    username: "@jjsmith",
+    name: 'Josh Smith',
+    username: '@jjsmith',
   },
   {
-    text: "This app has completely transformed how I manage my projects and deadlines.",
+    text: 'This app has completely transformed how I manage my projects and deadlines.',
     imageSrc: avatar3.src,
-    name: "Morgan Lee",
-    username: "@morganleewhiz",
+    name: 'Morgan Lee',
+    username: '@morganleewhiz',
   },
   {
-    text: "I was amazed at how quickly we were able to integrate this app into our workflow.",
+    text: 'I was amazed at how quickly we were able to integrate this app into our workflow.',
     imageSrc: avatar4.src,
-    name: "Casey Jordan",
-    username: "@caseyj",
+    name: 'Casey Jordan',
+    username: '@caseyj',
   },
   {
-    text: "Planning and executing events has never been easier. This app helps me keep track of all the moving parts, ensuring nothing slips through the cracks.",
+    text: 'Planning and executing events has never been easier. This app helps me keep track of all the moving parts, ensuring nothing slips through the cracks.',
     imageSrc: avatar5.src,
-    name: "Taylor Kim",
-    username: "@taylorkimm",
+    name: 'Taylor Kim',
+    username: '@taylorkimm',
   },
   {
-    text: "The customizability and integration capabilities of this app are top-notch.",
+    text: 'The customizability and integration capabilities of this app are top-notch.',
     imageSrc: avatar6.src,
-    name: "Riley Smith",
-    username: "@rileysmith1",
+    name: 'Riley Smith',
+    username: '@rileysmith1',
   },
   {
-    text: "Adopting this app for our team has streamlined our project management and improved communication across the board.",
+    text: 'Adopting this app for our team has streamlined our project management and improved communication across the board.',
     imageSrc: avatar7.src,
-    name: "Jordan Patels",
-    username: "@jpatelsdesign",
+    name: 'Jordan Patels',
+    username: '@jpatelsdesign',
   },
   {
-    text: "With this app, we can easily assign tasks, track progress, and manage documents all in one place.",
+    text: 'With this app, we can easily assign tasks, track progress, and manage documents all in one place.',
     imageSrc: avatar8.src,
-    name: "Sam Dawson",
-    username: "@dawsontechtips",
+    name: 'Sam Dawson',
+    username: '@dawsontechtips',
   },
   {
-    text: "Its user-friendly interface and robust features support our diverse needs.",
+    text: 'Its user-friendly interface and robust features support our diverse needs.',
     imageSrc: avatar9.src,
-    name: "Casey Harper",
-    username: "@casey09",
+    name: 'Casey Harper',
+    username: '@casey09',
   },
 ];
 
+const firstColumn = testimonials.slice(0, 3);
+const secondColumn = testimonials.slice(3, 6);
+const thirdColumn = testimonials.slice(6, 9);
+
+const TtestimonialsColumns = (props: {
+  className?: string;
+  testimonials: typeof testimonials;
+}) => {
+  return (
+    <div
+      className={twMerge(
+        'flex flex-col gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]',
+        props.className
+      )}
+    >
+      {props.testimonials.map(({ text, imageSrc, name, username }) => (
+        <div className='card' key={text}>
+          <div>{text}</div>
+          <div className='flex items-center gap-2 mt-5'>
+            <Image
+              src={imageSrc}
+              alt={name}
+              width={40}
+              height={40}
+              className='h-10 w-10 rounded-full'
+            />
+            <div className='flex flex-col'>
+              <p className='font-medium tracking-tight leading-5'>{name}</p>
+              <p className='leading-5 tracking-tight'>{username}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
 export const Testimonials = () => {
-  return null;
+  return (
+    <section className='bg-white py-0'>
+      <div className='container'>
+        <div className='section-header'>
+          <div className='flex justify-center'>
+            <div className='tag'>Testimonials</div>
+          </div>
+          <h2 className='section-title mt-5'>What Our Users Are Saying</h2>
+          <p className='section-description mt-5'>
+            From small businesses to large enterprises, our users have seen
+            significant improvements in their operations and productivity.
+          </p>
+        </div>
+        <div className='flex justify-center gap-6'>
+          <TtestimonialsColumns testimonials={firstColumn} />
+          <TtestimonialsColumns
+            testimonials={secondColumn}
+            className='hidden md:flex'
+          />
+          <TtestimonialsColumns
+            testimonials={thirdColumn}
+            className='hidden lg:flex'
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
